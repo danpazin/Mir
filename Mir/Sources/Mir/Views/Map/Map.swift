8bb3008e76dcef99f09f03bfe91a0b25709f2732
Daniil Pazin <52215503+danpazin@users.noreply.github.com>
2025-12-30 23:55:02 +0100

    Add Map and MetalView components with basic initial structure

---
+++ Mir/Sources/Mir/Views/Map/Map.swift
@@ -0,0 +1,17 @@
+// Map.swift
+//
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2025 Mir. All rights reserved.
+//
+
+import SwiftUI
+
+public struct Map: View {
+    public var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    Map()
+}
