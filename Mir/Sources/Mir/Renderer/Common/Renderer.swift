4e253870c92201b5c7b235b9fcb1b81bbb278e00
Daniil Pazin <52215503+danpazin@users.noreply.github.com>
2026-01-03 19:52:33 +0100

    Add MetalKit import and renderFrame method to Renderer protocol

--- Mir/Sources/Mir/Renderer/Common/Renderer.swift
+++ Mir/Sources/Mir/Renderer/Common/Renderer.swift
@@ -5,4 +5,13 @@
 // Copyright © 2026 Daniil Pazin. All rights reserved.
 //
 
-protocol Renderer: AnyObject { }
+import MetalKit
+
+protocol Renderer: AnyObject {
+    
+    /// Instructs the renderer to draw a frame for a view.
+    /// - Parameter view: A view the renderer draws to, which provides:
+    ///     - A render pass descriptor that reflects the view's current configuration
+    ///     - A drawable instance that the render draws to
+    func renderFrame(to view: MTKView)
+}
